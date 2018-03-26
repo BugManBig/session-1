a796a4121aa8b63816590e69d47db02fb8335e52
NanoByte <marseniym@gmail.com>
2018-03-26 12:25:57 +0300

    Check

--- src/main/java/ru/sbt/jschool/session1/Problem2.java
+++ src/main/java/ru/sbt/jschool/session1/Problem2.java
@@ -6,6 +6,8 @@ import java.util.Properties;
 /**
  * Created by Df-170815-F1 on 20.03.2018.
  */
+
+//Check
 public class Problem2 {
     public static void main(String[] args) {
         Properties p = System.getProperties();
